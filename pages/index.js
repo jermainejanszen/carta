fab6909ca723c70ecded02e58ae9fd7bd7004011
Jermaine Janszen <jermainejanszen@gmail.com>
2021-04-04 20:56:57 +1000

    began developing the splash screen

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Carta</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a>The Productivity Timekeeper</a>
         </h1>
 
         <p className="description">
