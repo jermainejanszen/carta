8c95528eaf1a04ef7bf0225a9b459ec11ddc17ab
Jermaine Janszen <jermainejanszen@gmail.com>
2021-06-26 23:56:06 +1000

    updated style for login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,31 +6,26 @@ interface Props {
 }
 
 const Login = (props: Props) => {
-    return (
-        
-        <div className={styles.container}>
-        <header className={styles.header}>
-        <SplashNavBar />
-        </header>
-        
-        <div className={styles.card}>
-            
-            <h2>Log in</h2>
-            
-            <form className={styles.form} action="">
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+          <SplashNavBar />
+      </header>
+      
+      <div className={styles.card}>
+          <h2>Log in</h2>
+          
+          <form className={styles.form} action="">
             <input placeholder="Username" type="text" name="Username" id="name"></input>
-            <br/>
-            <br/>
             <input placeholder="Password" type="text" name="Password" id="name"></input>
-            
-            </form>
-            <p className={styles.forgotPass}>forgot password?</p>
-
-            <button className={styles.button}>Log in</button>
-        </div>
-
-        </div>
-    )
+          </form>
+          
+          <p className={styles.forgotPass}>forgot password?</p>
+          
+          <button className={styles.button}>Log in</button>
+      </div>
+    </div>
+  )
 }
 
 export default Login
