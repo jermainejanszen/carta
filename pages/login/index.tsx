b83365b4f28ff054f39f3adf292d98ff9c9fae7d
Jermaine Janszen <jermainejanszen@gmail.com>
2021-06-26 20:10:35 +1000

    added login page

---
+++ pages/login/index.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Login.module.scss';
+
+interface Props {
+    
+}
+
+const Login = (props: Props) => {
+    return (
+        <div>
+            <h1>Alex code here</h1>
+        </div>
+    )
+}
+
+export default Login
