c74c397e16f040a41e9622db0c6f20bb2540f9e1
Jermaine Janszen <jermainejanszen@gmail.com>
2021-06-27 12:13:19 +1000

    updated input elements

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -6,31 +6,50 @@ interface Props {
 }
 
 const Register = (props: Props) => {
-    return (
-        <div className={styles.container}>
+  return (
+    <div className={styles.container}>
       <header className={styles.header}>
         <SplashNavBar />
       </header>
       
       <div className={styles.card}>
-          <h2>Register</h2>
-          
-          <form className={styles.form} action="" id="register-form">
-            <div>
-              <input placeholder="First Name" type="text" name="firstname"></input>
-              <input placeholder="Last Name" type="text" name="lastname"></input>
-            </div>
-            <input placeholder="Email Address" type="email" name="email"></input>
-            <input placeholder="Password" type="password" name="password"></input>
-            <input placeholder="Confirm Password" type="password" name="confirmpassword"></input>
-          </form>
-          
-          <p className={styles.forgotPass}>forgot password?</p>
-          
-          <input className={styles.button} type="button" form="register-form" value="Log in" />
+        <h2>Register</h2>
+        
+        <form className={styles.form} action="" id="register-form">
+          <div>
+            <input 
+              placeholder="First Name" 
+              type="text" 
+              name="firstname" 
+              autoCorrect="off" />
+            <input 
+              placeholder="Last Name" 
+              type="text" 
+              name="lastname" 
+              autoCorrect="off" />
+          </div>
+          <input 
+            placeholder="Email Address" 
+            type="email" 
+            name="email" />
+          <input 
+            placeholder="Password" 
+            type="password" 
+            name="password" 
+            autoComplete="off" />
+          <input 
+            placeholder="Confirm Password" 
+            type="password" 
+            name="confirmpassword" 
+            autoComplete="off" />
+        </form>
+        
+        <p className={styles.forgotPass}>forgot password?</p>
+        
+        <input className={styles.button} type="button" form="register-form" value="Register" />
       </div>
     </div>
-    )
+  )
 }
 
 export default Register
