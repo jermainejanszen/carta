c9e30e40c361b188bb51457da80c671941e40ae2
Jermaine Janszen <jermainejanszen@gmail.com>
2021-06-29 14:06:14 +1000

    added basic content for new project page

--- pages/projects/new/index.tsx
+++ pages/projects/new/index.tsx
@@ -1,4 +1,6 @@
 import ProfileNavBar from '../../../widgets/ProjectsNavBar';
+import Button from '../../../components/Button';
+
 import styles from '../../../styles/New.module.scss';
 
 interface Props {
@@ -13,7 +15,40 @@ const New = (props: Props) => {
       </header>
         
       <main className={styles.main}>
-        
+        <div className={styles.title}>
+          <h1>
+            Create a new project
+          </h1>
+          <p>
+            A project tracks the various tasks that are involved and provides a breakdown of the time you've spent on each one.
+          </p>
+        </div>
+        <div className={styles.content}>
+          <label htmlFor='name'>Project name</label>
+          <input 
+            className={styles.projectName} 
+            type='text' 
+            name='name'
+            autoComplete='off'
+            maxLength={80} />
+          <p>Great project names are short and memorable.</p>
+          <label htmlFor='description'>Description</label>
+          <input 
+            className={styles.projectDescription}
+            type='text' 
+            name='description'
+            autoComplete='off'
+            maxLength={300} />
+        </div>
+        <div>
+          <h2>
+            Add initial tasks
+          </h2>
+          <Button palette="secondary" variant="outlined">Add task</Button>
+        </div>
+        <div>
+          <Button palette="secondary">Create project</Button>
+        </div>
       </main>
     </div>
   )
