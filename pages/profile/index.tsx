b280ac0f39328825112b270f6a4395ce8dc11cf4
Jermaine Janszen <jermainejanszen@gmail.com>
2021-06-29 13:07:04 +1000

    added profile page

---
+++ pages/profile/index.tsx
@@ -0,0 +1,22 @@
+import ProfileNavBar from '../../widgets/ProjectsNavBar';
+import styles from '../../styles/Profile.module.scss';
+
+interface Props {
+    
+}
+
+const Profile = (props: Props) => {
+  return (
+    <div className={styles.container}>
+        <header className={styles.header}>
+          <ProfileNavBar />
+        </header>
+
+        <main className={styles.main}>
+          
+        </main>
+    </div>
+  )
+}
+
+export default Profile
