ec7f735dae6181f7f2bf5ed02e469bdc0e8f6b0d
alexandre roberge <roberge9912@gmail.com>
2021-07-02 23:32:58 +0400

    profile page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -13,7 +13,74 @@ const Profile = (props: Props) => {
         </header>
 
         <main className={styles.main}>
-          
+        <div className={styles.grid}>
+          <div>
+            <div className={styles.profilePic}>
+              <img src="/cofounder1.jpg" alt="pog" />
+            </div>
+
+            <div className={styles.userInfo}>
+              <h2>
+                Alexcrocrox
+              </h2>
+              <div className={styles.company}>
+                <img src="/icons/job.svg" alt="company" />
+                <h4>
+                  Carta
+                </h4>
+              </div>
+              <div className={styles.website}>
+                <img src="/icons/website.svg" alt="website" />
+                <h4>
+                  Carta.co
+                </h4>
+              </div>
+              <div className={styles.twitter}>
+                <img src="/icons/twitter.svg" alt="website" />
+              <h4>
+                alexcrocrox@twitter.com
+              </h4>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.stats}>
+            <div className={styles.statsGroup}>
+              <div>
+                <h2>
+                  Personal statistics
+                </h2>
+                  <div className={`${styles.tasks} ${styles.statsGroup}`}>
+                    <img src="/icons/tick.svg" alt="nike" />
+                    <div>
+                      <h3>
+                        15
+                      </h3>
+                      <p>Tasks completed</p>
+                    </div>
+                  </div>
+                  <div className={`${styles.projComp} ${styles.statsGroup}`}>
+                    <img src="/icons/project.svg" alt="nike" />
+                    <div>
+                      <h3>
+                        23
+                      </h3>
+                      <p>Projects completed</p>
+                    </div>
+                  </div>
+                  <div className={`${styles.timeSpent} ${styles.statsGroup}`}>
+                    <img src="/icons/time.svg" alt="nike" />
+                    <div>
+                      <h3>
+                        23hrs
+                      </h3>
+                      <p>Total time spent</p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
         </main>
     </div>
   )
