81a1c24f11179a8166062d775e1d4710dc5a2665
Jermaine Janszen <jermainejanszen@gmail.com>
2021-07-08 13:10:52 +1000

    updated register button

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,4 +1,5 @@
 import SplashNavBar from "../../widgets/SplashNavBar";
+import Button from "../../components/Button";
 import styles from "../../styles/Register.module.scss";
 
 interface Props {
@@ -6,6 +7,11 @@ interface Props {
 }
 
 const Register = (props: Props) => {
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -15,8 +21,12 @@ const Register = (props: Props) => {
       <div className={styles.card}>
         <h2>Register</h2>
         
-        <form className={styles.form} action="" id="register-form">
-          <div>
+        <form 
+          className={styles.form} 
+          action="" 
+          id="register-form">
+
+          <div className={styles.formName}>
             <input 
               placeholder="First Name" 
               type="text" 
@@ -28,6 +38,7 @@ const Register = (props: Props) => {
               name="lastname" 
               autoCorrect="off" />
           </div>
+
           <input 
             placeholder="Email Address" 
             type="email" 
@@ -42,11 +53,16 @@ const Register = (props: Props) => {
             type="password" 
             name="confirmpassword" 
             autoComplete="off" />
+        
+          <p className={styles.forgotPass}>forgot password?</p>
+
+          <Button
+            palette="primary"
+            onClick={handleRegister}>
+            Register
+          </Button>
+
         </form>
-        
-        <p className={styles.forgotPass}>forgot password?</p>
-        
-        <input className={styles.button} type="button" form="register-form" value="Register" />
       </div>
     </div>
   )
