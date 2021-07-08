b108b4a9d69676597f21eb5f7e1b9b640a472c59
Jermaine Janszen <jermainejanszen@gmail.com>
2021-07-08 12:28:21 +1000

    updated button on login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import SplashNavBar from '../../widgets/SplashNavBar';
+import Button from '../../components/Button';
 import styles from '../../styles/Login.module.scss';
 
 interface Props {
@@ -10,7 +11,8 @@ const Login = (props: Props) => {
 
   const router = useRouter();
 
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
     router.push('/projects');
   }
 
@@ -23,19 +25,23 @@ const Login = (props: Props) => {
       <div className={styles.card}>
         <h2>Log in</h2>
         
-        <form className={styles.form} action="" id="login-form">
+        <form 
+          className={styles.form} 
+          action="" 
+          id="login-form" 
+          onSubmit={handleLogin}>
+            
           <input placeholder="Username" type="text" name="username" />
           <input placeholder="Password" type="password" name="password" />
+        
+          <p className={styles.forgotPass}>forgot password?</p>
+          
+          <Button
+            palette="primary"
+            onClick={handleLogin} >
+              Log in
+          </Button>
         </form>
-        
-        <p className={styles.forgotPass}>forgot password?</p>
-        
-        <input 
-          className={styles.button} 
-          type="button" 
-          form="login-form" 
-          value="Log in"
-          onClick={handleLogin} />
       </div>
     </div>
   )
