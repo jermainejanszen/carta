a14e345fa511b197224ceca75baa0c5cff06faf6
Jermaine Janszen <jermainejanszen@gmail.com>
2021-07-09 21:29:14 +1000

    implemented chakra in login

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,24 +27,28 @@ export default function Home() {
       displayName: "CollaborateIcon",
       viewBox: "0 0 33 33",
       path: [
-        (<path 
-          fill-rule="evenodd" 
-          clip-rule="evenodd" 
+        (<path
+          key="1"
+          fillRule="evenodd" 
+          clipRule="evenodd" 
           d="M6.6 24.75C8.87818 24.75 10.725 22.9032 10.725 20.625C10.725 18.3468 8.87818 16.5 6.6 16.5C4.32183 16.5 2.475 18.3468 2.475 20.625C2.475 22.9032 4.32183 24.75 6.6 24.75ZM3.8147e-06 29.7C3.8147e-06 27.6455 0.938699 25.8103 2.41059 24.5998C3.4629 25.7086 4.95071 26.4 6.6 26.4C8.24929 26.4 9.7371 25.7086 10.7894 24.5998C12.2613 25.8103 13.2 27.6455 13.2 29.7V33H3.8147e-06V29.7Z" 
           fill="currentColor"/>),
         (<path 
-          fill-rule="evenodd" 
-          clip-rule="evenodd" 
+          key="2"
+          fillRule="evenodd" 
+          clipRule="evenodd" 
           d="M26.4 8.25C28.6782 8.25 30.525 6.40317 30.525 4.125C30.525 1.84683 28.6782 0 26.4 0C24.1218 0 22.275 1.84683 22.275 4.125C22.275 6.40317 24.1218 8.25 26.4 8.25ZM19.8 13.2C19.8 11.1455 20.7387 9.3103 22.2106 8.09984C23.2629 9.20858 24.7507 9.89997 26.4 9.89997C28.0493 9.89997 29.5371 9.20858 30.5894 8.09984C32.0613 9.3103 33 11.1455 33 13.2V16.5H19.8V13.2Z" 
           fill="currentColor"/>),
         (<path 
-          fill-rule="evenodd" 
-          clip-rule="evenodd" 
+          key="3"
+          fillRule="evenodd" 
+          clipRule="evenodd" 
           d="M26.9 29.7V19.8H25.9V29.2H19.8V26.4L16.5 29.7L19.8 33V30.2H26.4H26.9V29.7Z" 
           fill="currentColor"/>),
         (<path 
-          fill-rule="evenodd" 
-          clip-rule="evenodd" 
+          key="4"
+          fillRule="evenodd" 
+          clipRule="evenodd" 
           d="M13.2 3.8V6.6L16.5 3.3L13.2 0V2.8H6.6H6.1V3.3V13.2H7.1V3.8H13.2Z" 
           fill="currentColor"/>),
       ],
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { 
+  Text, 
+  Flex, 
+  VStack, 
+  Input,
+  InputGroup,
+  InputRightElement, 
+  Button,
+  useColorModeValue,
+  FormControl,
+  FormLabel,
+  FormHelperText} from '@chakra-ui/react';
 import SplashNavBar from '../../widgets/SplashNavBar';
-import Button from '../../components/Button';
 import styles from '../../styles/Login.module.scss';
 
 interface Props {
@@ -9,6 +21,7 @@ interface Props {
 
 const Login = (props: Props) => {
 
+  const [showPass, setShowPass] = useState(false);
   const router = useRouter();
 
   const handleLogin = (e) => {
@@ -16,34 +29,72 @@ const Login = (props: Props) => {
     router.push('/projects');
   }
 
+  const bg = useColorModeValue("whiteAlpha.900", "gray.700");
+
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
+    <Flex
+      direction="column"
+      alignItems="center"
+      minH="100vh">
         <SplashNavBar />
-      </header>
-      
-      <div className={styles.card}>
-        <h2>Log in</h2>
         
-        <form 
-          className={styles.form} 
-          action="" 
-          id="login-form" 
-          onSubmit={handleLogin}>
+        <Flex 
+          flexGrow={1}
+          direction="column"
+          justifyContent="center">
+            <Flex 
+              direction="column"
+              alignItems="center"
+              padding="10"
+              border="1px solid gray"
+              borderRadius="xl"
+              bg={bg}>
+              <Text fontSize="5xl">
+                Log in
+              </Text>
             
-          <input placeholder="Username" type="text" name="username" />
-          <input placeholder="Password" type="password" name="password" />
-        
-          <p className={styles.forgotPass}>forgot password?</p>
-          
-          <Button
-            palette="primary"
-            onClick={handleLogin} >
-              Log in
-          </Button>
-        </form>
-      </div>
-    </div>
+              <form
+                action=""
+                onSubmit={handleLogin}>
+            
+                <Flex direction="column">
+                  <VStack spacing="6" my="2rem">
+                    <FormControl>
+                      <FormLabel>Username</FormLabel>
+                      <Input placeholder="Username" />
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel>Password</FormLabel>
+                      <InputGroup size="md">
+                        <Input
+                          pr="4.5rem"
+                          type={showPass ? "text" : "password"}
+                          placeholder="Enter password"
+                        />
+                        <InputRightElement width="4.5rem">
+                          <Button h="1.75rem" size="sm" onClick={() => setShowPass(!showPass)}>
+                            {showPass ? "Hide" : "Show"}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      <FormHelperText 
+                        cursor="pointer">
+                          Forgot password?
+                      </FormHelperText>
+                    </FormControl>
+                  </VStack>
+
+                  <Button
+                    variant="primary"
+                    onClick={handleLogin}
+                    alignSelf="center">
+                      Log in
+                  </Button>
+                </Flex>
+              </form>
+            </Flex>
+        </Flex>
+    </Flex>
   )
 }
 
