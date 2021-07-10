aa7a920ab53c3d739929ad0103a533839480980c
Jermaine Janszen <jermainejanszen@gmail.com>
2021-07-10 12:04:54 +1000

    implemented chakra in projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from 'next/router';
-
+import { 
+  Text, 
+  Grid,
+  VStack,
+  useColorModeValue } from '@chakra-ui/react';
+import { FiPlusCircle } from 'react-icons/fi';
 import ProfileNavBar from '../../widgets/ProjectsNavBar';
 import ProjectCard from '../../components/ProjectCard';
-
-import styles from '../../styles/Projects.module.scss';
 
 const mockData = [
   {
@@ -41,27 +44,60 @@ const Projects = (props: Props) => {
 
   const router = useRouter();
 
+  const titleBg = useColorModeValue("#D7FAFA", "gray.900");
+  const newProjectBg = useColorModeValue(
+    "linear-gradient(to-r, #FAC584, #FF8C80)", 
+    "linear-gradient(to-r, cyan.800, blue.800)");
+
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
+    <VStack
+      direction="column"
+      alignItems="center"
+      minH="100vh">
         <ProfileNavBar />
-      </header>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Projects</h1>
+        <VStack 
+          direction="column"
+          maxW="100rem"
+          width="100%"
+          padding="7rem">
+            <Text 
+              fontSize="5xl"
+              w="fit-content"
+              p="4"
+              mb="8"
+              borderRadius="xl"
+              bg={titleBg} >
+                Projects
+            </Text>
 
-        <div className={styles.grid}>
-          <div 
-            className={styles.newProject}
-            onClick={() => router.push('/projects/new')}>
-            <h1>New Project</h1>
-            <img src='/icons/plus.svg' alt='plus' />
-          </div>
-          {mockData.map((value, index) => <ProjectCard data={value} color={index} key={index} />)}
-        </div>
-      </main>
-      
-    </div>
+            <Grid
+              width="100%"
+              templateColumns="repeat(auto-fill, 20rem)"
+              gap="2rem" >
+                <VStack
+                  spacing="1rem"
+                  alignItems="center"
+                  p="3rem"
+                  borderRadius="xl"
+                  cursor="pointer"
+                  color="white"
+                  bgGradient={newProjectBg}
+                  transition="all 0.2s ease-in-out"
+                  onClick={() => router.push('/projects/new')}
+                  _hover={{
+                    transform: "scale(1.05)",
+                    boxShadow: "0 0 10px rgba(0, 0, 0, 0.4)"
+                  }}>
+                    <Text fontSize="3xl">
+                      New Project
+                    </Text>
+                    <FiPlusCircle size="8rem" />
+                </VStack>
+                {mockData.map((value, index) => <ProjectCard data={value} color={index} key={index} />)}
+            </Grid>
+        </VStack>
+    </VStack>
   )
 }
 
