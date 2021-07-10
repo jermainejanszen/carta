11444e788acf32f451f8a9ba86c5635a58e09f40
Jermaine Janszen <jermainejanszen@gmail.com>
2021-07-10 17:00:47 +1000

    converted first row of profile page to chakra

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,3 +1,23 @@
+import { 
+  Text, 
+  Flex, 
+  Grid,
+  Box,
+  Image,
+  Button,
+  useColorModeValue, 
+  Divider, 
+  Input,
+  FormLabel,
+  FormHelperText, 
+  FormControl,
+  Textarea, 
+  VStack,
+  InputGroup, 
+  InputRightElement,
+  IconButton,
+  HStack, } from '@chakra-ui/react';
+import { FiCheckCircle, FiCalendar, FiClock } from 'react-icons/fi';
 import ProfileNavBar from '../../widgets/ProjectsNavBar';
 import styles from '../../styles/Profile.module.scss';
 
@@ -7,99 +27,110 @@ interface Props {
 
 const Profile = (props: Props) => {
   return (
-    <div className={styles.container}>
-        <header className={styles.header}>
-          <ProfileNavBar hideSearch />
-        </header>
+    <Flex 
+      direction="column"
+      alignItems="center"
+      minH="100vh" >
+        <ProfileNavBar hideSearch />
 
-        <main className={styles.main}>
-        <div className={styles.grid}>
-          <div className={styles.row1}>
-            <div className={styles.profilePic}>
-              <img src="/cofounder1.jpg" alt="pog" />
-            </div>
-              <div>
-                <div className={styles.bio}>
-                  <h2>
-                    Alexcrocrox
-                  </h2>
-                  <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo maxime distinctio debitis voluptate incidunt corporis earum quod iure.
-                  </p>
-                </div>
-                
-              </div>
-              <div className={styles.stats}>
-            <div className={styles.statsGroup}>
-              <div>
-                <h2>
+        <Grid
+          templateColumns="repeat(5, minmax(0, 1fr))"
+          gap="2rem"
+          p="7rem"
+          maxW="90rem"
+          width="100%" >
+            <Box
+              gridColumn="1 / span 1" >
+              <Image
+                borderRadius="3xl"
+                src="/cofounder1.jpg" 
+                alt="pog" />
+            </Box>
+
+            <Box
+              gridColumn="2 / span 2" >
+                <Text fontSize="3xl">
+                  Jerchael
+                </Text>
+                <Text>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo maxime distinctio debitis voluptate incidunt corporis earum quod iure.
+                </Text>
+            </Box>
+
+            <Box
+              gridColumn="4 / span 2" >
+                <Text fontSize="2xl">
                   Personal statistics
-                </h2>
-                  <div className={`${styles.tasks} ${styles.statsGroup}`}>
-                    <img src="/icons/tick.svg" alt="nike" />
-                    <div>
-                      <h3>
+                </Text>
+                <VStack width="100%" alignItems="flex-start">
+                  <HStack>
+                    <FiCheckCircle size={42} />
+                    <Box>
+                      <Text fontSize="xl">
                         15
-                      </h3>
-                      <p>Tasks completed</p>
-                    </div>
-                  </div>
-                  <div className={`${styles.projComp} ${styles.statsGroup}`}>
-                    <img src="/icons/project.svg" alt="nike" />
-                    <div>
-                      <h3>
+                      </Text>
+                      <Text>
+                        Tasks completed
+                      </Text>
+                    </Box>
+                  </HStack>
+                  <HStack>
+                    <FiCalendar size={42} />
+                    <Box>
+                      <Text fontSize="xl">
                         23
-                      </h3>
-                      <p>Projects completed</p>
-                    </div>
-                  </div>
-                  <div className={`${styles.timeSpent} ${styles.statsGroup}`}>
-                    <img src="/icons/time.svg" alt="nike" />
-                    <div>
-                      <h3>
+                      </Text>
+                      <Text>
+                        Projects completed
+                      </Text>
+                    </Box>
+                  </HStack>
+                  <HStack>
+                    <FiClock size={42} />
+                    <Box>
+                      <Text fontSize="xl">
                         23hrs
-                      </h3>
-                      <p>Total time spent</p>
+                      </Text>
+                      <Text>
+                        Total time spent
+                      </Text>
+                    </Box>
+                  </HStack>
+                </VStack>
+            </Box>
+
+              <div className={styles.row2}>
+                <div className={styles.userInfo}>
+                    <h3>
+                      Links
+                    </h3>
+                    <div className={styles.company}>
+                      <img src="/icons/job.svg" alt="company" />
+                      <h4>
+                        Carta
+                      </h4>
+                    </div>
+                    <div className={styles.website}>
+                      <img src="/icons/website.svg" alt="website" />
+                      <h4>
+                        Carta.co
+                      </h4>
+                    </div>
+                    <div className={styles.twitter}>
+                      <img src="/icons/twitter.svg" alt="website" />
+                    <h4>
+                      alexcrocrox@twitter.com
+                    </h4>
                     </div>
                   </div>
+                  <div className={styles.graph}>
+                    <h3>
+                      Insights
+                    </h3>
+                  </div>
               </div>
-            </div>
-          </div>
-          </div>
-
-          <div className={styles.row2}>
-            <div className={styles.userInfo}>
-                <h3>
-                  Links
-                </h3>
-                <div className={styles.company}>
-                  <img src="/icons/job.svg" alt="company" />
-                  <h4>
-                    Carta
-                  </h4>
-                </div>
-                <div className={styles.website}>
-                  <img src="/icons/website.svg" alt="website" />
-                  <h4>
-                    Carta.co
-                  </h4>
-                </div>
-                <div className={styles.twitter}>
-                  <img src="/icons/twitter.svg" alt="website" />
-                <h4>
-                  alexcrocrox@twitter.com
-                </h4>
-                </div>
-              </div>
-              <div className={styles.graph}>
-                <h3>
-                  Insights
-                </h3>
-              </div>
-          </div>
-          </div>
-        </main>
-    </div>
+        </Grid>
+    </Flex>
   )
 }
 
